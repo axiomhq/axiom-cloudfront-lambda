885697536cb0b51045c346c61b94b984a6866d74
Arne Bahlo <arne@axiom.co>
2022-05-10 08:38:12 +0200

    Add skeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/axiomhq/axiom-cloudfront-lambda
 
 go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.31.1
+	github.com/axiomhq/axiom-go v0.9.1
+	github.com/axiomhq/pkg v0.3.0
+	go.uber.org/zap v1.21.0
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/axiomhq/axiom-go/axiom"
+	"github.com/axiomhq/pkg/cmd"
+	"go.uber.org/zap"
+)
+
 func main() {
-	println("Hello world")
+	cmd.Run("axiom-cloudwatch-lambda", run,
+		cmd.WithRequiredEnvVars("AXIOM_DATASET"),
+		cmd.WithValidateAxiomCredentials(),
+	)
+}
+
+func run(ctx context.Context, _ *zap.Logger, client *axiom.Client) error {
+	// Export `AXIOM_TOKEN`, `AXIOM_ORG_ID` and `AXIOM_DATASET` for Axiom Cloud.
+	// Export `AXIOM_URL`, `AXIOM_TOKEN` and `AXIOM_DATASET` for Axiom Selfhost.
+
+	hf := handler(client, os.Getenv("AXIOM_DATASET"))
+	lambda.StartWithContext(ctx, hf)
+
+	return nil
+}
+
+func handler(client *axiom.Client, dataset string) func(context.Context, events.S3Event) error {
+	return func(ctx context.Context, logsEvent events.S3Event) error {
+		return errors.New("unimplemented")
+	}
 }
